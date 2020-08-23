67b85e6bdda998dac96340d4a6020158b231cfcc
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2020-08-23 15:12:58 +0200

    add Spring Boot App with test

---
+++ src/test/java/com/gabriel/msscssm/MsscSsmApplicationTest.java
@@ -0,0 +1,14 @@
+package com.gabriel.msscssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class MsscSsmApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
